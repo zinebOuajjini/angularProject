07219647db7e8fa2ddb5f2cc1ea9ff987f3a7708
zineb <ouajjinizineb4@gmail.com>
2018-04-18 16:28:29 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatFormField, MatInput,MatOption,MatSelect} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+
+    BrowserModule,
+    MatFormField,
+    MatInput,
+    MatOption,
+    MatSelect,
+    BrowserAnimationsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
